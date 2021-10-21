54bb889979bb73edd4cc5e6dee4a6ff615dc214e
“saravana]” <saravansai002@gmail.com>
2021-10-21 12:23:55 +0530

    titte changed

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 3 | Dashboard</title>
+  <title>AS TRADERS | Dashboard</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php 
