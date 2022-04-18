ade05c9be556debcec53672f1d03f95ea28f8088
Saravana Sai <36000435+saravanasai@users.noreply.github.com>
2022-04-18 13:56:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AS TRADERS | Dashboard</title>
+  <title>AS TRADERS | Dashboards</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php 
